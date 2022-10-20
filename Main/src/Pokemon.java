9bc44ed3e656f148dc4af1e776099a063a0dee2c
DarkS34 <masthaker@gmail.com>
2022-10-20 13:15:53 +0200

    act prueba

--- Main/src/Pokemon.java
+++ Main/src/Pokemon.java
@@ -1,5 +1,5 @@
 public class Pokemon {
     public static void main(String[] args) throws Exception {
-        System.out.println("Helloa, World!3");
+        System.out.println("Helloas, World!3");
     }
 }
