588d9c6af84dcf849442e2f7a0ef41b8420e5c76
DarkS34 <masthaker@gmail.com>
2022-11-16 02:25:39 +0100

    MovimientoOleksandr

--- Main/src/Bulbasor.java
+++
@@ -1,6 +0,0 @@
-public class Bulbasor{
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Bulbasor, arroz con pollo");
-
-    }
-}
--- Main/src/Charmander.java
+++
@@ -1,8 +0,0 @@
-public class Charmander {
-    private int hp;
-    private int ataque;
-    private boolean pedo;
-    private int lore;
-    private int prueba;
-    private String dato;
-}
